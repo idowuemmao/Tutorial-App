a56d3a54837319d505d11a88c2a3a0e6c6c5bc24
Idowu Emmanuel <idowuemmao@gmail.com>
2023-07-07 16:26:58 +0100

    API CALLS

--- src/ApiCall/GetData.js
+++ src/ApiCall/GetData.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function GetData() {
   const [starWarsData, setStarWarsData] = useState({});
-  fetch("https://swapi.dev/api/people/1")
-    .then((res) => res.json())
-    // .then((data) => setStarWarsData(data));
-    .then((data) => console.log(data));
+  const [count, setCount] = useState(1);
+
+  useEffect(function () {
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+    // .then((data) => console.log(data));
+  }, [count]);
 
   return (
-    <div>
+    <div className="inline-grid place-items-center gap-4">
+      <h1>The Count is {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+        className="bg-red-500 p-1 shadow-lg border border-black"
+      >
+        Get Next Character
+      </button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
